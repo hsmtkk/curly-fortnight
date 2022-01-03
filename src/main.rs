db6f90825bf816fafd71425b027449b3f3ad6d13
hsmtkk <hsmtkk@gmail.com>
2022-01-03 16:58:40 +0900

    add callback endpoint

--- src/main.rs
+++ src/main.rs
@@ -16,11 +16,18 @@ async fn main() {
     info!("start server at {}", addr);
 
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new()
+    .route("/", get(|| async { "Hello, World!" }))
+    .route("/callback", get(callback));
 
     // run it with hyper on localhost:3000
     axum::Server::bind(&addr.parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+async fn callback() -> String {
+    info!("callback");
+    "OK".to_string()
 }
